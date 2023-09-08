f0cb63d733d102fe2cd7d23c8c0b704bc2fe1554
GlacierLuo <1090490148@qq.com>
2023-09-09 00:21:27 +0800

    优化网络请求为并行。

--- src/features/input.ts
+++ src/features/input.ts
@@ -2,52 +2,65 @@ import { TplFeature } from '@/types/utools'
 import request from '@/Helper/HttpHelper'
 import { decodeAbiParameters, parseAbiParameters } from 'viem'
 
+async function multiQuery (input) {
+  let sigStr = input.substr(0, 10)
+  let res: any = await request.get(`https://api.openchain.xyz/signature-database/v1/lookup?function=${sigStr}&filter=true`)
+  let functionName: string = res.result.function[Object.keys(res.result.function)[0]][0].name
+  let args = functionName.match(/\(.+\)$/g)[0].replace('(', '').replace(')', '')
+  let argsList = args.split(',')
+  return {
+    functionName,
+    args,
+    argsList,
+    input
+  }
+}
+
 async function decodeInput(input, callback) {
   try {
     let list = []
     let inputList = [input]
 
     while (inputList.length !== 0) {
-      let tempInput = inputList.pop()
+      let queryList = []
+      while (inputList.length !== 0){
+        queryList.push(multiQuery(inputList.pop()))
+      }
+      let res = await Promise.all(queryList)
 
-      let sigStr = tempInput.substr(0, 10)
-      let sig: any = await request.get(`https://api.openchain.xyz/signature-database/v1/lookup?function=${sigStr}&filter=true`)
-      let functionName: string = sig.result.function[sigStr][0].name
-      let args = functionName.match(/\(.+\)$/g)[0].replace('(', '').replace(')', '')
-      let argsList = args.split(',')
-
-      list.push({
-        title: `${functionName}`,
-        description: `Function Name`,
-        icon: '' // 图标(可选)
-      })
-
-      let data = decodeAbiParameters(
-        parseAbiParameters(args),
-        tempInput.replace(tempInput.slice(0, 10), '0x')
-      )
-      data.map((v, i, arr) => {
-        if (functionName.startsWith('multicall')) {
-          if (v instanceof Array) {
-            v.map((v, i, arr) => {
-              inputList.push(v)
-            })
-          }
-        }
-
+      for(let item of res) {
         list.push({
-          title: `${v}`,
-          description: argsList[i].match(/[a-z]+/g)[0],
+          title: `${item.functionName}`,
+          description: `Function Name`,
           icon: '' // 图标(可选)
         })
-      })
+
+        let data = decodeAbiParameters(
+          parseAbiParameters(item.args as string),
+          item.input.replace(item.input.slice(0, 10), '0x')
+        )
+        data.map((v, i, arr) => {
+          if (item.functionName.startsWith('multicall')) {
+            if (v instanceof Array) {
+              v.map((v, i, arr) => {
+                inputList.push(v)
+              })
+            }
+          }
+  
+          list.push({
+            title: `${v}`,
+            description: item.argsList[i].match(/[a-z]+/g)[0],
+            icon: '' // 图标(可选)
+          })
+        })
+      }
     }
 
     callback(list)
   } catch (e) {
-    console.log(e)
-    // utools.showNotification('Decode failed!')
-    // utools.outPlugin()
+    utools.showNotification('Decode failed!')
+    utools.outPlugin()
   }
 }
 
